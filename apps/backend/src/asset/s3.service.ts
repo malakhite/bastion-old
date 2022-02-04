3a7d9c18f4ef64bd0764d71cc19a9d5c32f2cb2f
Scott Abbey <scott@scottabbey.com>
2022-02-03 21:21:51 -0500

    s3 service working

--- apps/backend/src/asset/s3.service.ts
+++ apps/backend/src/asset/s3.service.ts
@@ -1,5 +1,5 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuid } from 'uuid';
 
@@ -9,20 +9,16 @@ export class S3Service {
 
 	async uploadFile(dataBuffer: Buffer, filename: string) {
 		const Key = `${uuid()}-${filename}`;
-		try {
-			const client = new S3Client({ region: 'us-east-1' });
-			const uploadResult = await client.send(
-				new PutObjectCommand({
-					Bucket: this.configService.get('aws.bucket_name'),
-					Key,
-					Body: dataBuffer,
-				}),
-			);
-			console.log(uploadResult);
+		const client = new S3Client({ region: 'us-east-1' });
+		const uploadResult = await client.send(
+			new PutObjectCommand({
+				Bucket: this.configService.get('s3.bucket_name'),
+				Key,
+				Body: dataBuffer,
+			}),
+		);
+		console.log(uploadResult);
 
-			return Key;
-		} catch (maybeError) {
-			throw new BadRequestException(maybeError);
-		}
+		return Key;
 	}
 }
