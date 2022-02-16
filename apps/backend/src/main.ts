50c5669ad2212d60275810d5dc2c4bb083055325
Scott Abbey <sabbey@homes.com>
2022-02-16 13:41:14 -0500

    Add pino for logging

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger, VersioningType } from '@nestjs/common';
+import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { bufferLogs: true });
+	app.useLogger(app.get(Logger));
 	const globalPrefix = 'api';
 	app.setGlobalPrefix(globalPrefix);
 	app.enableVersioning({
@@ -20,9 +17,6 @@ async function bootstrap() {
 	const host = config.get('HOST');
 	const port = config.get('PORT');
 	await app.listen(port, host);
-	Logger.log(
-		`🚀 Application is running on: http://${host}:${port}/${globalPrefix}`,
-	);
 }
 
 bootstrap();
