dbd99d17b2534c8b936505392753ba4e66d490db
Scott Abbey <sabbey@homes.com>
2022-03-04 16:03:36 -0500

    start work on frontend

---
+++ apps/frontend/pages/_app.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import {
+	ColorScheme,
+	ColorSchemeProvider,
+	MantineProvider,
+} from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+// import '../styles/index.scss';
+
+export default function CustomApp({ Component, pageProps }: AppProps) {
+	const userColorScheme = useColorScheme();
+	const [colorScheme, setColorScheme] =
+		useState<ColorScheme>(userColorScheme);
+
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+	return (
+		<>
+			<Head>
+				<title>Welcome to frontend!</title>
+			</Head>
+			<main className="app">
+				<ColorSchemeProvider
+					colorScheme={colorScheme}
+					toggleColorScheme={toggleColorScheme}
+				>
+					<MantineProvider
+						withGlobalStyles
+						withNormalizeCSS
+						theme={{
+							colorScheme,
+						}}
+					>
+						<Component {...pageProps} />
+					</MantineProvider>
+				</ColorSchemeProvider>
+			</main>
+		</>
+	);
+}
