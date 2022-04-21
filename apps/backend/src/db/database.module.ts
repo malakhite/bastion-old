e4d746811bfeae3af87d632eb060d6008c38e64a
Scott Abbey <scott@scottabbey.com>
2022-04-21 08:24:09 -0400

    dockerfile updates

--- apps/backend/src/db/database.module.ts
+++ apps/backend/src/db/database.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { readFileSync } from 'fs';
+import { Role } from '../user/entities/role.entity';
+import { User } from '../user/entities/user.entity';
 
 @Module({
 	imports: [
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => ({
-				type: 'postgres',
-				url: configService.get('DATABASE_URL'),
-				synchronize: Boolean(configService.get('SYNCHRONIZE_DB')),
-				autoLoadEntities: true,
-			}),
+			useFactory: function dbFactory(configService: ConfigService) {
+				return {
+					type: 'postgres',
+					url: configService.get('DATABASE_URL'),
+					synchronize: Boolean(configService.get('SYNCHRONIZE_DB')),
+					entities: [User, Role],
+					ssl: {
+						ca: readFileSync('./bastion-do.crt').toString(),
+					},
+				};
+			},
 		}),
 	],
 })
